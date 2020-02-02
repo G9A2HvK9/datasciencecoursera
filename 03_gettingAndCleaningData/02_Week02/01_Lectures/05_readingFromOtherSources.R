2048d294f7653535119f475e3e6ea723d594cf14
g9a2hvk9 <g9a2hvk9@gmail.com>
2020-02-02 14:23:08 +0100

    adds 5th lecture from week 2

---
+++ 03_gettingAndCleaningData/02_Week02/01_Lectures/05_readingFromOtherSources.R
@@ -0,0 +1,21 @@
+## Useful packages
+# google --> 'data storage mechanism R package'
+# example: 'mySQL R package'
+
+# file or url functions to download files or open connections
+# ? connections helps with more info
+
+# foreign package helpe read other languages
+# read.FILEEXTENSION to read other programming languages
+
+# Postgres MySQL, Microsoft Access all have compliantb packages
+# RMongo allows reading from Mongo DB
+
+# You can read images as well
+# jpeg, readbitmap, png, EBImage are the relevant packages
+
+# GIS Data (Geographic information system)
+# rdgal. rgeos, raster
+
+# Audio files
+# tuneR, seewave
