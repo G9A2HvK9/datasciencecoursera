55ba0352a166a6fe3a3c35b1446382fcb3b651fc
g9a2hvk9 <g9a2hvk9@gmail.com>
2020-02-25 11:05:26 +0100

    Adds lecture 1 from course 4

--- 04_exploratoryDataAnalysis/01_lectures/01_principlesOfAnalyticGraphs.R
+++ 04_exploratoryDataAnalysis/01_lectures/01_principlesOfAnalyticGraphs.R
@@ -4,3 +4,35 @@
 ## 1. Principle: Show Comparisons
 ## Evidence is always relative <- 'compared to what??'
 ## Effect of air cleaner on astha sympoms <- 'compared to what??'
+
+## 2. Principle: Show causality
+## Tell a story about how the world works, according to your data
+## Effect of air cleaner on asthma syomptoms <- Effect of air cleaner on particulate matter
+## Effect of particulate matter on asthma symptoms reduced through air cleaner
+
+## 3. Principle: Show multivariate data
+## Life is inherently multivariate <- more data tells a more holistic story
+## Important to show as many variables as is reasonable in your data, so you can get a clear picture
+
+## 4. Principle: Integration of Evidence
+## Integrate words, numbers, images, any source of informative data
+## Your data presentation is presentation of evidence. You tell a story
+## Anything that helps you tell the story is good
+## Don't let your tools dictate your presentation
+
+## 5. Principle: Describe and Document Your Evidence
+## Credible evidence tells a more credible story
+## Lend credibility to your evidence by documenting and describing your sources
+## Include direct sources, computer code etc
+
+## 6. Principle: Content is King
+## No matter how good your graph, a bad story is boring
+## Interesting data or an interesting question is key
+
+## All principles:
+## 1. Show comparisons
+## 2. Show causality / a mechanism
+## 3. Show multivariate data
+## 4. Integrate multiple modes of evidence
+## 5. Describe and Document the Evidence
+## 6. Content is King
