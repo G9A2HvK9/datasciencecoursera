a2ab76e413eeea011a5dfa36728cdc1bf61a120d
g9a2hvk9 <g9a2hvk9@gmail.com>
2020-02-25 08:40:53 +0100

    Adds course 4 - exploratory data analysis

---
+++ 04_exploratoryDataAnalysis/01_lectures/01_principlesOfAnalyticGraphs.R
@@ -0,0 +1,6 @@
+## Rules for how you tell stories through data
+## Book: Beautiful Evidence by Edward Tufte
+
+## 1. Principle: Show Comparisons
+## Evidence is always relative <- 'compared to what??'
+## Effect of air cleaner on astha sympoms <- 'compared to what??'
